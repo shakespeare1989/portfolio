e73e68ae5414ca5c338e63f61073caced3a4d210
Autumn Shakespeare <autumnshakespeare@Autumns-MacBook-Pro.local>
2021-01-10 17:55:15 -0700

    second test

---
+++ build/precache-manifest.062cc91c014e94a82189631ada91f849.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "40734e44909ececf8ef4",
+    "url": "/static/css/main.dcdefb21.chunk.css"
+  },
+  {
+    "revision": "40734e44909ececf8ef4",
+    "url": "/static/js/main.40734e44.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "b71fba2b067433d28d52c80f28d410d9",
+    "url": "/index.html"
+  }
+];
